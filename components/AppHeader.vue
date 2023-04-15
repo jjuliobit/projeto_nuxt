3dfa4bcccba9fe610b319acae936886fa0bd82e2
jjuliobit <stenghelj@gmail.com>
2023-04-15 11:55:49 -0300

    :)

---
+++ components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app-bar class="bar-nav">
+        <v-container>
+            <v-row>
+                <v-col auto>
+                    <v-btn id="btn-nav" large rounded>
+                        <span>Video</span>
+                    </v-btn>
+                </v-col>
+                <v-col auto>
+                    <v-btn id="btn-nav"  large rounded>
+                        <span>Music</span>
+                    </v-btn>
+                </v-col>
+                <v-col auto>
+                    <v-btn id="btn-nav"  large rounded>
+                        <span>Book</span>
+                    </v-btn>
+                </v-col>
+                <v-col auto>
+
+                    <v-btn class="btn-nav"  large rounded>
+                        <span>Image</span>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app-bar>
+</template>
+
+
+<style lang="scss">
+@import '../assets/styles/header.scss';
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-else-if>
+      <AppHeader />
+      <AppFooter />
+    </v-else-if>
+</template>
+
+<script setup>
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
+</script>
+
