23b3e06adf1c8db269e9318ed192e52cd09a3aa2
jjuliobit <stenghelj@gmail.com>
2023-04-28 19:07:19 -0300

    Bootstrap just now

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,39 +1,40 @@
 <template>
-    <div>
-        <v-navigation-drawer v-model="drawer" clipped :fixed="$vuetify.breakpoint.mdAndDown" app>
-        </v-navigation-drawer>
-        <v-app-bar clipped-left app :fixed="true">
-            <v-app-bar-nav-icon v-if="$vuetify.breakpoint.mdAndDown" @click="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Logo</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-menu left bottom>
-                <template v-slot:activator="{ on }">
-                    <v-btn icon v-on="on">
-                       <MenuIcon />
-                    </v-btn>
-                </template>
-                <v-list>
-                    <v-list-item @click="$vuetify.theme.dark = !$vuetify.theme.dark" v-model="success">
-                        <v-list-item-title>Dark Mode</v-list-item-title>
-                        <mdicon name="react" />
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-        </v-app-bar>
-    </div>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#"><b-avatar size='sm'></b-avatar></b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
-<script>
-import MenuIcon from 'vue-material-design-icons/Menu.vue';
-export default {
-    name: "Default",
-    data: () => ({
-        drawer: null,
-        success: null
-    }),
-    components: {
-        MenuIcon,
-    }
-}
-</script>
